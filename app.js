6601fb012f55cc5fafb5a7bd70cdb1c687850f79
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-05-29 15:06:39 -0700

    User Schemas

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express = require('express')
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var passport = require('passport')
+var localStrategy = require('passport-local')
+var passportLocalMongoose = require('passport-local-mongoose')
+
+var User = require('./models/user.js')
 
 var app = express()
+
+app.use(require('express-session')({
+  secret: "why me",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 mongoose.connect('mongodb://localhost/auth-demo')
 app.set('view engine', 'ejs')
